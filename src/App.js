fd67731c6a7c8f5441c8e84bddd83eb03dc416af
raffyqul <raffyqulazzam@gmail.com>
2024-06-11 20:28:54 +0700

    update--page-event

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import "./App.css";
 import About from './pages/About';
 import Home from "./pages/Home";
+import Event from "./pages/Event"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/event' element={<Event />} />
       </Routes>
     </Router>
   )
--- src/pages/event/Content.jsx
+++ src/pages/event/Content.jsx
@@ -2,6 +2,8 @@ import ImageCard1 from "../../assets/images/home/event/event-1.png";
 import ImageCard2 from "../../assets/images/home/event/event-2.png";
 import ImageCard3 from "../../assets/images/home/event/event-3.png";
 import ImageCard4 from "../../assets/images/home/event/event-4.png";
+import ImageCard5 from "../../assets/images/event/event1.png";
+import ImageCard6 from "../../assets/images/event/event2.png";
 import IconCalender from "../../assets/images/home/event/icon-calender.png";
 import IconLocation from "../../assets/images/home/event/icon-loc.png";
 
@@ -9,7 +11,7 @@ export default function Content() {
   return (
     <section className="bg-[#E9ECEF] py-[72px]">
       <div className="container mx-auto">
-        <div className="event flex flex-col gap-12">
+        <div className="event flex flex-col gap-12  mb-16">
           <div className="content-top flex flex-col gap-2 items-center">
             <h2>Event Wayang</h2>
             <span className="text-base text-gray text-center max-w-[402px]">
@@ -106,9 +108,93 @@ export default function Content() {
                   </div>
                 </div>
               </div>
+              <div className="card flex flex-col gap-2">
+                <img src={ImageCard5} alt="" className="card-img" />
+                <div className="card-body flex flex-col gap-2">
+                  <h4 className="text-base font-semibold text-darkBlack">
+                    Pesta Wayang 2023 - “Mewayang Hayu Eksistensi & Konservasi”
+                  </h4>
+                  <div className="wrapper">
+                    <div className="wrap flex items-center gap-1">
+                      <img src={IconCalender} alt="" className="max-w-[24px]" />
+                      <span className="text-sm text-lightBlack font-medium">
+                        3 Februari 2024
+                      </span>
+                    </div>
+                    <div className="wrap flex items-center gap-1">
+                      <img src={IconLocation} alt="" className="max-w-[24px]" />
+                      <span className="text-sm text-lightBlack font-medium">
+                        Sidoarjo
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card flex flex-col gap-2">
+                <img src={ImageCard6} alt="" className="card-img" />
+                <div className="card-body flex flex-col gap-2">
+                  <h4 className="text-base font-semibold text-darkBlack">
+                    Pesta Wayang 2023 - “Mewayang Hayu Eksistensi & Konservasi”
+                  </h4>
+                  <div className="wrapper">
+                    <div className="wrap flex items-center gap-1">
+                      <img src={IconCalender} alt="" className="max-w-[24px]" />
+                      <span className="text-sm text-lightBlack font-medium">
+                        3 Februari 2024
+                      </span>
+                    </div>
+                    <div className="wrap flex items-center gap-1">
+                      <img src={IconLocation} alt="" className="max-w-[24px]" />
+                      <span className="text-sm text-lightBlack font-medium">
+                        Sidoarjo
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <button className="btn btn-primary">Lihat Event Lainnya</button>
           </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <nav aria-label="Pagination">
+            <ul className="pagination">
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#" aria-label="Previous">
+                  <span aria-hidden="true">Sebelumnya</span>
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link active" href="/#">
+                  1
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#">
+                  2
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#">
+                  3
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#">
+                  4
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#">
+                  5
+                </a>
+              </li>
+              <li className="pagination-item">
+                <a className="pagination-link" href="/#" aria-label="Next">
+                  <span aria-hidden="true">Selanjutnya</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </section>
